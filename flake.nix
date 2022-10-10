0cff25acb49b3b40131211ddd27bce03e0a2a85b
Douglas Liberalesso <dliberalesso@gmail.com>
2022-10-10 19:39:01 +0000

    Add a formatter

--- flake.nix
+++ flake.nix
@@ -1,6 +1,20 @@
 {
   description = "My Neovim config";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cachix.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://dliberalesso.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "dliberalesso.cachix.org-1:7qs1S5Qd766dYFU86nVux/wRMZ8UEUbhn3Qxp/TwiOc="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
 
@@ -16,8 +30,11 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        formatter = pkgs.nixpkgs-fmt;
+
         devShell = with pkgs; mkShell {
           buildInputs = [
+            nixpkgs-fmt
             rnix-lsp
           ];
         };
