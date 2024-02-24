3d74762c8f0d095c9d8d5ea9038f1c3f161810fa
Douglas Liberalesso <dliberalesso@gmail.com>
2024-02-24 20:49:30 +0000

    Extract options to their own file

--- config/default.nix
+++ config/default.nix
@@ -2,15 +2,8 @@
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
+    ./options.nix
   ];
-
-  config = {
-    options = {
-      number = true;         # Show line numbers
-      relativenumber = true; # Show relative line numbers
-      shiftwidth = 2;        # Tab width should be 2
-    };
-  };
 
   editorconfig.enable = true;
 
