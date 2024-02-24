c8ada630f98472963f9fe81d71aedf719fc6a0a6
Douglas Liberalesso <dliberalesso@gmail.com>
2024-02-24 20:46:55 +0000

    Add Dracula as a theme; enable editorconfig, lualine and treesitter; set number and shiftwidth.

--- config/default.nix
+++ config/default.nix
@@ -3,4 +3,21 @@
   imports = [
     ./bufferline.nix
   ];
+
+  config = {
+    options = {
+      number = true;         # Show line numbers
+      relativenumber = true; # Show relative line numbers
+      shiftwidth = 2;        # Tab width should be 2
+    };
+  };
+
+  editorconfig.enable = true;
+
+  plugins = {
+    lualine.enable = true;
+    treesitter.enable = true;
+  };
+
+  colorschemes.dracula.enable = true;  
 }
