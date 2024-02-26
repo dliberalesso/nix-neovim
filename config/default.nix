3a713164bb707f18927611e79152e760fe21747d
Douglas Liberalesso <dliberalesso@gmail.com>
2024-02-26 15:50:28 +0000

    Sync clipboard with OS and disable mouse how-to popup

--- config/default.nix
+++ config/default.nix
@@ -5,12 +5,20 @@
     ./options.nix
   ];
 
+  colorschemes.dracula.enable = true;
+
   editorconfig.enable = true;
+
+  # Sync clipboard between OS and Neovim
+  clipboard.register = "unnamedplus";
 
   plugins = {
     lualine.enable = true;
     treesitter.enable = true;
   };
 
-  colorschemes.dracula.enable = true;  
+  extraConfigVim = ''
+    aunmenu PopUp.How-to\ disable\ mouse
+    aunmenu PopUp.-1-
+  '';
 }
