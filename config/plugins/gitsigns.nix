75b7e16fadc72aee7d8c8bdc90cde6b0cd3b477f
Douglas Liberalesso <dliberalesso@gmail.com>
2024-05-08 21:30:01 +0000

    Change gitsign text

--- config/plugins/gitsigns.nix
+++ config/plugins/gitsigns.nix
@@ -3,11 +3,12 @@
     enable = true;
 
     settings.signs = {
-      add.text = "+";
-      change.text = "~";
-      delete.text = "_";
+      add.text = "│";
+      change.text = "│";
+      delete.text = "󰍵";
       topdelete.text = "‾";
       changedelete.text = "~";
+      untracked.text = "│";
     };
   };
 }
