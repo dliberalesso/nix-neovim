e334ab4ee34cc4a25d4d5fe07c22347c88b65505
Douglas Liberalesso <dliberalesso@gmail.com>
2024-06-20 20:18:38 +0000

    feat(utils): added `diffview.nvim`

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,6 +1,28 @@
 ---@type LazySpec[]
 return {
   {
+    "nix/diffview.nvim",
+
+    -- TODO: add a custom event like
+    -- event = "User AstroFile",
+    -- event = { "BufReadPost", "BufNewFile" },
+
+    cmd = { "DiffviewOpen" },
+
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { winbar_info = true },
+        file_history = { winbar_info = true },
+      },
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.b[bufnr].view_activated = false
+        end,
+      },
+    },
+  },
+  {
     "nix/vim-startuptime",
     cmd = "StartupTime",
     init = function()
