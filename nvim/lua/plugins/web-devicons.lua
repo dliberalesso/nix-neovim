8ddb894041da56a14dd326a4b13eb2d9aad38000
Douglas Liberalesso <dliberalesso@gmail.com>
2024-06-24 15:15:00 +0000

    feat(icons): overrided some `web-devicons` default icons

---
+++ nvim/lua/plugins/web-devicons.lua
@@ -0,0 +1,24 @@
+---@return LazySpec
+return {
+  dir = vim.env.LAZY_ROOT_DIR .. "/nvim-web-devicons",
+
+  config = function()
+    require("nvim-web-devicons").setup({
+      override = {
+        default_icon = { icon = require("core.icons").DefaultFile },
+        deb = { icon = "", name = "Deb" },
+        lock = { icon = "󰌾", name = "Lock" },
+        mp3 = { icon = "󰎆", name = "Mp3" },
+        mp4 = { icon = "", name = "Mp4" },
+        out = { icon = "", name = "Out" },
+        ["robots.txt"] = { icon = "󰚩", name = "Robots" },
+        ttf = { icon = "", name = "TrueTypeFont" },
+        rpm = { icon = "", name = "Rpm" },
+        woff = { icon = "", name = "WebOpenFontFormat" },
+        woff2 = { icon = "", name = "WebOpenFontFormat2" },
+        xz = { icon = "", name = "Xz" },
+        zip = { icon = "", name = "Zip" },
+      },
+    })
+  end,
+}
