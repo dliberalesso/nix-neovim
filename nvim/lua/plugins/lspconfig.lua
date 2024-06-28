d3d99cbd12356c8f17651b28c1dc9e3e5ed5c14c
Douglas Liberalesso <dliberalesso@gmail.com>
2024-06-28 19:30:13 +0000

    fix(lspconfig): do not use `astrocore` in `cmd` function of plugin spec

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -12,16 +12,17 @@ return {
     { dir = vim.env.LAZY_ROOT_DIR .. "/neoconf.nvim", opts = {} },
   },
   cmd = function(_, cmds) -- HACK: lazy load lspconfig on `:Neoconf` if neoconf is available
-    if require("astrocore").is_available("neoconf.nvim") then table.insert(cmds, "Neoconf") end
+    if require("lazy.core.config").spec.plugins["neoconf.nvim"] then table.insert(cmds, "Neoconf") end
     vim.list_extend(cmds, { "LspInfo", "LspLog", "LspStart" }) -- add normal `nvim-lspconfig` commands
   end,
   event = "User AstroFile",
   config = function(_, _)
     local setup_servers = function()
       vim.tbl_map(require("astrolsp").lsp_setup, require("astrolsp").config.servers)
-      require("astrocore").exec_buffer_autocmds("FileType", { group = "lspconfig" })
 
-      require("astrocore").event("LspSetup")
+      local astrocore = require("astrocore")
+      astrocore.exec_buffer_autocmds("FileType", { group = "lspconfig" })
+      astrocore.event("LspSetup")
     end
     local astrocore = require("astrocore")
     if astrocore.is_available("mason-lspconfig.nvim") then
